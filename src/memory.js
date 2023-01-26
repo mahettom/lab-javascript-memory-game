083a4fdf96b33eff8b58903a0ba7b2cdd5bad092
TomMahe <mahettom@gmail.com>
2023-01-26 17:59:17 +0100

    problem with itteration shuffle

--- src/memory.js
+++ src/memory.js
@@ -1,15 +1,31 @@
 class MemoryGame {
-  constructor(cards) {
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0; 
+    this.pairsGuessed = 0;
+    }
+
+  shuffleCards(array) {
+
+    if (!this.cards){
+      return undefined;
+    }
+
+    const shuffleArray = [];
+
+    this.cards.forEach(card => {
+    let shuffle = this.cards[Math.floor(Math.random() * (this.cards.length))];
+    shuffleArray += shuffle;
+    });
+
+    //this.cards += shuffleArray
+    console.log(shuffleArray)
+    return shuffleArray;
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
-
-  checkIfPair(card1, card2) {
-    // ... write your code here
+  checkIfPair(nameCard1, nameCard2) {
+    
   }
 
   checkIfFinished() {
